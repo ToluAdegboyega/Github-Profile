01c701e6a116c7d04f5fd463c87e759d5f782bec
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-11-16 11:54:22 +0100

    chore: try new.

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 fetch(URL, {
   method: "POST",
   headers: {
-    Authorization: "Bearer   repo-token",
+    Authorization: "Bearer  API_TOKEN",
     "Content-Type": "application/json",
   },
   body: JSON.stringify({
